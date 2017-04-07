9df9b8dc58869c7703ab8b0ab78430bb5dc492d5
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-07 12:41:10 -0700

    -iframe + jquery nevim jestli funguje jde spustit pouze na serveru

--- public/stats.py
+++ public/stats.py
@@ -9,4 +9,4 @@ for entry in log:
 	if 'map.py' in entry:
 		num += 1
 
-print "<p>Tento nástroj vygeneroval již " + str(num) + " map.</p>"
+print "<p id="stat">Tento nástroj vygeneroval již " + str(num) + " map.</p>"
