39aa7b3f8a544c2c5e1257fe8e67f140bd59beb7
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-04-07 16:02:57 +0200

    Partial revert due to the binary

--- public/statistics.py
+++ public/statistics.py
@@ -1,7 +1,1 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-import os
-
-CMD = "./visitors -A -o html --grep 'map.py' ~/access.log 2>/dev/null "
-os.system(CMD)
+../statistics.py
---
+++ statistics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+
+CMD = "./visitors -A -o html --grep 'map.py' ~/access.log 2>/dev/null "
+os.system(CMD)
