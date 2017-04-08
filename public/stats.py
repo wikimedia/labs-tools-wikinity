229fa83683d4782127b64ddb0d64c7db076d8ff8
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-04-08 10:02:37 +0200

    Print HTML header in stats.py

--- public/stats.py
+++ public/stats.py
@@ -8,5 +8,14 @@ num = 0
 for entry in log:
 	if 'map.py' in entry:
 		num += 1
-
-print "<p id="stat">Tento nástroj vygeneroval již " + str(num) + " map.</p>"
+print """
+<!DOCTYPE html>
+<html lang="cs-cz">
+        <head>
+                <meta charset="utf-8" />
+	</head>
+	<body>
+		<p id="stat">Tento nástroj vygeneroval již """ + str(num) + """ map.</p>
+	</body>
+</html>
+"""
