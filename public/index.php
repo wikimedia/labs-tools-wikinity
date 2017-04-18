f6c126a03492a20d52e6578c73f83015abe6ef7b
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-18 03:19:53 -0700

    fix

--- public/index.php
+++ public/index.php
@@ -46,7 +46,8 @@ $I18N->registerDomain( 'wikinity', __DIR__ . '/../messages' );
       <a class="navbar-brand" href="https://github.com/urbanecm/wikinity/" style="color: #fff;float: right">
           <i class="fa fa-github fa-lg" aria-hidden="true"></i> GitHub - zdrojový kód
       </a>
-            <form class="navbar-brand" style="float: right; color: black">
+    <a style="float: right" class="navbar-brand" href="https://github.com/urbanecm/wikinity/issues/new">Nahlásit problém</a>
+    <form class="navbar-brand" style="float: right; color: black">
         <select class="custom-select">
         <?php
         $langs = $I18N->getAvailableLangs();
@@ -66,7 +67,6 @@ $I18N->registerDomain( 'wikinity', __DIR__ . '/../messages' );
         ?>
         </select>
       </form>
-      <a style="float: right" class="navbar-brand" href="https://github.com/urbanecm/wikinity/issues/new">Nahlásit problém</a>
   </nav>
   
 <!-- Forms -->
