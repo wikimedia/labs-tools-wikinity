225b1f489b8149daf53ba05ac45b23b062e87bf0
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-18 03:16:31 -0700

    fix

--- public/index.php
+++ public/index.php
@@ -33,6 +33,10 @@ $I18N->registerDomain( 'wikinity', __DIR__ . '/../messages' );
         font-size:150%;
         text-decoration:underline;
     }
+
+    .no-hover { 
+        color: black;
+    }
   </style>
 </head>
 <body>
@@ -42,7 +46,7 @@ $I18N->registerDomain( 'wikinity', __DIR__ . '/../messages' );
       <a class="navbar-brand" href="#" style="color: #fff"> 
         Wikinity
       </a>
-      <form class="navbar-brand" style="float: right">
+      <form class="navbar-brand no-hover" style="float: right">
         <select class="custom-select">
         <?php
         $langs = $I18N->getAvailableLangs();
@@ -62,7 +66,7 @@ $I18N->registerDomain( 'wikinity', __DIR__ . '/../messages' );
         ?>
         </select>
       </form>
-      <a style="float: right" class="navbar-brand" href="https://github.com/urbanecm/wikinity/issues/new">Nahlásit problém</a>
+      <a style="float: right" class="navbar-brand no-hover" href="https://github.com/urbanecm/wikinity/issues/new">Nahlásit problém</a>
       <a class="navbar-brand" href="https://github.com/urbanecm/wikinity/" style="color: #fff;float: right">
           <i class="fa fa-github fa-lg" aria-hidden="true"></i> GitHub - zdrojový kód
       </a>
