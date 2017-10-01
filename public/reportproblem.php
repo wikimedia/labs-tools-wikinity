637c41b1030d11464b0e1ff3f41c14a76157a579
Martin Urbanec <martin@urbanec.cz>
2017-10-01 18:10:36 +0200

    Move navbar to navbar.php

--- public/reportproblem.php
+++ public/reportproblem.php
@@ -1,16 +1,6 @@
 <?php include 'header.php'; ?>
 <body>
-	<!-- navbar -->
-	<nav class="navbar navbar-inverse bg-faded" style="background-color:#337ab7;border-color: 2e6da4; border-radius: 0;">
-		<a class="navbar-brand" href="#" style="color: #fff">
-		  Wikinity
-		</a>
-
-		<a class="navbar-brand" href="https://github.com/urbanecm/wikinity/" style="color: #fff;float: right">
-			<i class="fa fa-github fa-lg" aria-hidden="true"></i> GitHub - zdrojový kód
-		</a>
-	  <a style="float: right; color: white;" class="navbar-brand no-hover" href="reportproblem.php">Nahlásit problém</a>
-	</nav>
+	<?php include 'navbar.php'; ?>
 	<div class="container">
 		<form action="issue.py" method="post">
 			<div class="row">
