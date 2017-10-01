6338c9bc1f916f08724ed5c75a79e7ee05289dad
Martin Urbanec <martin@urbanec.cz>
2017-10-01 15:52:25 +0200

    Add basic s.py
    It will be used for getting to shortened addresses

---
+++ public/s.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import yaml
+config = yaml.load(open('config.yaml'))
+from wmflabs import db
+conn = db.connect()
+import sys
+import os
+import cgi
+import cgitb
+
+if 'QUERY_STRING' in os.environ:
+	QS = os.environ['QUERY_STRING']
+	qs = cgi.parse_qs(QS)
+	id = qs.keys()[0]
+else:
+	print 'Add something to query string'
+	sys.exit()
+
+print id
