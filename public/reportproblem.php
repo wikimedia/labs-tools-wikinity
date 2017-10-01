53895e5c34af57ff65298875896c6d07c16b0bb2
Martin Urbanec <martin@urbanec.cz>
2017-10-01 18:16:29 +0200

    More i18n

--- public/reportproblem.php
+++ public/reportproblem.php
@@ -5,20 +5,20 @@
 		<form action="issue.py" method="post">
 			<div class="row">
 				<div class="col">
-					<label for="email">Váš e-mail</label><br>
+					<label for="email"><?php echo $I18N->msg("your-email"); ?></label><br>
 					<input type="text" name="email" id="email" value="">
 				</div>
 				<div class="col">
-					<label for="title">Shrnutí problému</label><br>
+					<label for="title"><?php echo $I18N->msg("problem-summary"); ?></label><br>
 					<input type="text" name="title" id="title" value="">
 				</div>
 				<div class="col">
-					<label for="body">Podrobnější popis</label><br>
+					<label for="body"><?php echo $I18N->msg("problem-body"); ?></label><br>
 					<textarea name="body" rows="8" cols="80"></textarea>
 				</div>
 			</div>
 			<div class="col-3">
-				  <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+				  <button type="submit" class="btn btn-primary" id="submit"><?php echo $I18N->msg("submit"); ?></button>
 			</div>
 		</form>
 	</div>
