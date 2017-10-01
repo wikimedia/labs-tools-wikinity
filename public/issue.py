9846faa63c43c1e83ebcebda3d64287e36ba7ee0
Martin Urbanec <martin@urbanec.cz>
2017-10-01 14:20:45 +0200

    Introduce issue.py

---
+++ public/issue.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+import sys
+import os
+import cgi
+
+if 'QUERY_STRING' in os.environ:
+	QS = os.environ['QUERY_STRING']
+	qs = cgi.parse_qs(QS)
+	try:
+		email = qs['email'][0]
+		title = qs['title'][0]
+		body = qs['body'][0]
+	except:
+		sys.exit()
+else:
+	sys.exit()
+
+header = """
+<!DOCTYPE html>
+<html lang="cs-cz">
+        <head>
+                <meta charset="utf-8" />
+"""
+tail = """
+        </body>
+</html>
+"""
+print header
+print email
+print title
+print body
+print tail
+
+sys.exit()
+
+
+sender = email
+recipient = "bugs@webappky.cz"
+
+mail = "!projects #wikinity\n\n" + body
+msg = MIMEText(mail)
+msg['Subject'] = title
+msg['From'] = sender
+msg['To'] = recipient
+s = smtplib.SMTP('mail.tools.wmflabs.org')
+s.ehlo()
+s.sendmail(sender, recipient, msg.as_string())
+s.quit()
